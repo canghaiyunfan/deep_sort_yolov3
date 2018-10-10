87032825a888709637c8ddbce76252de6703652e
zhaoyi <huashuizhaoyi@163.com>
2018-10-10 16:23:10 +0800

    添加注释

--- yolo3/utils.py
+++ yolo3/utils.py
@@ -21,8 +21,10 @@ def letterbox_image(image, size):
     w, h = size
     new_w = int(image_w * min(w*1.0/image_w, h*1.0/image_h))
     new_h = int(image_h * min(w*1.0/image_w, h*1.0/image_h))
+    #将原图进行resize
     resized_image = image.resize((new_w,new_h), Image.BICUBIC)
 
+    #新生成图片，将新生成的图片粘贴到resize后的图片上
     boxed_image = Image.new('RGB', size, (128,128,128))
     boxed_image.paste(resized_image, ((w-new_w)//2,(h-new_h)//2))
     return boxed_image
