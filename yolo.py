ff4bf2cff71fda897d1f5204b66baa1d1e6143cd
zhaoyi <huashuizhaoyi@163.com>
2018-10-11 09:32:03 +0800

    解决Exception ignored in: <bound method BaseSession.__del__ of <tensorflow.python.client.session.Session object at 0x7fd3edd13e10>>

--- yolo.py
+++ yolo.py
@@ -27,6 +27,7 @@ class YOLO(object):
         self.iou = 0.5
         self.class_names = self._get_class()
         self.anchors = self._get_anchors()
+        K.clear_session()
         self.sess = K.get_session()
         self.model_image_size = (416, 416) # fixed size or (None, None)
         self.is_fixed_size = self.model_image_size != (None, None)
